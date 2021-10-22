714463ac4e6b1e2175dea5a836e97fac5015d9d3
CristianVilla2000 <85590379+CristianVilla2000@users.noreply.github.com>
2021-10-22 14:17:36 -0500

    Spring 5

--- tiendavirtual/src/main/webapp/scripts/producto.js
+++ tiendavirtual/src/main/webapp/scripts/producto.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
 		var getUrl = window.location;
 		var baseUrl = getUrl.protocol + "//"+getUrl.host+"/"+getUrl.pathname.split('/')[1];
 			
-				
 		form_data.append("file", file_data);
 		$.ajax({
 	            type: "POST",
